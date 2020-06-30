766b78f7b968dabbe6671ae15f1b9edef6d2e2ea
ahsanshekih <60756564+ahsanshekih@users.noreply.github.com>
2020-07-01 00:26:48 +0100

    Update r.sh

--- r.sh
+++ r.sh
@@ -18,7 +18,7 @@ MESSAGE="Late at work. "$RANDOM_REASON
 # Send a text message
 RESPONSE=`curl -fSs -u "$AC1f3efe8cce99cd91d0f1e55a16f74edd:$4795e16db192cf6a745f8ccdc550bd9a" \
   -d "From=$MY_NUMBER" -d "To=$HER_NUMBER" -d "Body=$MESSAGE" \
-  "https://api.twilio.com/2010-04-01/Accounts/$TWILIO_ACCOUNT_SID/Messages"`
+  "https://api.twilio.com/2010-04-01/Accounts/$AC1f3efe8cce99cd91d0f1e55a16f74edd/Messages"`
 
 # Log errors
 if [ $? -gt 0 ]; then
